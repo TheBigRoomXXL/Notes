90e334bb425a2c8690d077b7bd84d7105e796c58
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-09-25 18:06:46 +0200

    add templating

--- src/server.go
+++ src/server.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	view "notes/src/views"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.Static("/", "src/static")
+	e.Renderer = view.T
+	e.Static("/static", "src/views/static")
+	e.GET("/", Hello)
 
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Info(e.Start(":3000"))
+}
+
+func Hello(c echo.Context) error {
+	err := c.Render(http.StatusOK, "index.html", "World")
+	fmt.Println(err)
+	return err
 }
