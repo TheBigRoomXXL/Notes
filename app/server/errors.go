708f895754a9c6b06221e825f6c4017b1169f5e8
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-11-19 19:18:33 +0100

    feat (errorHandler): hide internal details by default

--- app/server/errors.go
+++ app/server/errors.go
@@ -8,27 +8,28 @@ import (
 
 // specification: https://www.rfc-editor.org/rfc/rfc9457.html
 type RFC9457ProblemDetails struct {
-	Type    string `json:"type,omitempty"`    // url to documentation on the error
-	Title   string `json:"title,omitempty"`   // A short, human-readable summary of the type.
-	Status  int    `json:"status,omitempty"`  // The HTTP status code
-	Details string `json:"details,omitempty"` //A human-readable explanation specific to this occurrence of the problem.
+	Type     string `json:"type,omitempty"`    // url to documentation on the error
+	Title    string `json:"title,omitempty"`   // A short, human-readable summary of the type.
+	Status   int    `json:"status,omitempty"`  // The HTTP status code
+	Details  string `json:"details,omitempty"` //A human-readable explanation specific to this occurrence of the problem.
+	internal error  // Original error for record keeping
 }
 
 func (pb *RFC9457ProblemDetails) Error() string {
-	return pb.Details
+	return pb.internal.Error()
 }
 
 func RFC9457ErrorHandler(err error, c echo.Context) {
-	status := http.StatusInternalServerError
-	if he, ok := err.(*echo.HTTPError); ok {
-		status = he.Code
-	}
-	c.Logger().Error(err)
+	c.Logger().Error(err.Error())
 
-	msg := RFC9457ProblemDetails{
-		Status:  status,
-		Details: err.Error(),
+	if pb, ok := err.(*RFC9457ProblemDetails); ok {
+		c.Logger().Error(pb.Error())
+		c.JSON(pb.Status, pb)
+		return
 	}
 
-	c.JSON(status, msg)
+	c.JSON(http.StatusInternalServerError, RFC9457ProblemDetails{
+		Status: http.StatusInternalServerError,
+		Title:  "Internal Server Error",
+	})
 }
